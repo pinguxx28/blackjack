f706aeee8ed2c6cdff5f3de53278ba365aee7b25
pinguxx28 <riskumax@gmail.com>
2023-05-15 19:32:30 +0300

    add win/lose

--- src/main.c
+++ src/main.c
@@ -19,13 +19,45 @@ int main(int argc, char *argv[])
 	hand dealer = { 0 };
 	hand player = { 0 };
 
-	for (int n = 0; n < 2; n++) {
+	for (int n = 0; n < 2; n++)
 		get_card(&player);
+	
+	while (dealer.total_value < 17)
 		get_card(&dealer);
-	}
 
-	printf("dealer: %d\n", dealer.total_value);
+	printf("dealer: %d + x\n", dealer.hand[0].value);
 	printf("player: %d\n", player.total_value);
+
+	int hit = 1;
+	char option;
+	while (hit == 1) {
+		scanf("%c", &option);
+		hit = option == 'h' ? 1 : 0;
+		if (!hit) break;
+
+		get_card(&player);
+		printf("%d\n", player.hand[player.hand_p-1].value);
+	}
+	
+	printf("-- result --\n");
+
+	printf("dealer: ");
+	for (int i = 0; i < dealer.hand_p; i++)
+		printf("%d ", dealer.hand[i].value);
+	printf(" = %d\n", dealer.total_value);
+
+	printf("player: %d\n", player.total_value);
+	
+	if (player.total_value > 21)
+		printf("Player busts, you lose!\n");
+	else if (dealer.total_value > 21)
+		printf("Dealer busts, you win!\n");
+	else if (player.total_value > dealer.total_value)
+		printf("You win!\n");
+	else if (player.total_value == dealer.total_value)
+		printf("It's a push!\n");
+	else if (player.total_value < dealer.total_value)
+		printf("You lose!\n");
 	
 
 
